ab2deeff1232b498143775e8e06efb30f5de3606
Danny Wade <danny.lee.wade@gmail.com>
2022-06-18 11:11:48 +0100

    Initial Commit

---
+++ MyTasks/MyTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTasksApp.swift
+//  MyTasks
+//
+//  Created by Danny Wade on 18/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
